bb43e825b0cb2195f614900e19a6758f84e2110a
mlucas <mlucas@codavel.com>
2020-11-23 23:49:11 +0000

    Fixed icon size and anchors

--- js/fishmap.js
+++ js/fishmap.js
@@ -77,37 +77,37 @@ var settings = {
 
         settings.iconBarbel = L.icon({
             iconUrl: 'media/icon_barbel.png',
-            iconSize: [20, 20],
-            iconAnchor: [10, 10],
-            popupAnchor: [10, 10]
+            iconSize: [30, 20],
+            iconAnchor: [15, 10],
+            popupAnchor: [15, 10]
         });
 
         settings.iconPike = L.icon({
             iconUrl: 'media/icon_pike.png',
-            iconSize: [20, 20],
-            iconAnchor: [10, 10],
-            popupAnchor: [10, 10]
+            iconSize: [30, 20],
+            iconAnchor: [15, 10],
+            popupAnchor: [15, 10]
         });
 
         settings.iconSeaLamprey = L.icon({
             iconUrl: 'media/icon_lamprey.png',
-            iconSize: [20, 20],
-            iconAnchor: [10, 10],
-            popupAnchor: [10, 10]
+            iconSize: [30, 20],
+            iconAnchor: [15, 10],
+            popupAnchor: [15, 10]
         });
 
         settings.iconShad = L.icon({
             iconUrl: 'media/icon_shad.png',
-            iconSize: [20, 20],
-            iconAnchor: [10, 10],
-            popupAnchor: [10, 10]
+            iconSize: [30, 20],
+            iconAnchor: [15, 10],
+            popupAnchor: [15, 10]
         });
 
         settings.iconZander = L.icon({
             iconUrl: 'media/icon_zander.png',
-            iconSize: [20, 20],
-            iconAnchor: [10, 10],
-            popupAnchor: [10, 10]
+            iconSize: [30, 20],
+            iconAnchor: [15, 10],
+            popupAnchor: [15, 10]
         });
 
         placeWeirMarkers();
@@ -1257,9 +1257,9 @@ function getMovingMarkerIcon(current_icon, mirrored) {
 
     let icon = L.icon({
         iconUrl: iconURL,
-        iconSize: [20, 20],
-        iconAnchor: [10, 10],
-        popupAnchor: [10, 10]
+        iconSize: [30, 20],
+        iconAnchor: [15, 10],
+        popupAnchor: [15, 10]
     });
     return icon;
 }
